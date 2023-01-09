297daae48ef02b48b311269136bd13eac3701978
ZerrY <suhan0304@gmail.com>
2023-01-09 16:25:57 +0900

    Create 개인정보_수집_유효기간.py
    23.01.09/level1
    개인정보 수집 유효기간

---
+++ Level1/개인정보_수집_유효기간.py
@@ -0,0 +1,16 @@
+import re
+
+def solution(today, terms, privacies):
+    answer = []
+    term = {}
+    today = list(map(int, today.split('.')))
+    days = (today[0]-2000) * 28 * 12 + today[1] * 28 + today[2]
+    print("days = ",days)
+    for t in terms :
+        term[re.split(' ',t)[0]] = int(re.split(' ',t)[1])
+    for privacy in privacies :
+        p = re.split('[ .]',privacy)
+        p[:3] = list(map(int,p[:3]))
+        p_days = ((p[0]-2000) * 28 * 12 + p[1] * 28 + p[2]) + term[p[3]] * 28 
+        print("privacy days = ",p_days)
+    return answer
