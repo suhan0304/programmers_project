a45403588a4ce835058cebedd5a2597969e91ecd
ZerrY <suhan0304@gmail.com>
2023-01-10 16:13:58 +0900

    Update 이모티콘 할인행사.py

--- Level2/이모티콘 할인행사.py
+++ Level2/이모티콘 할인행사.py
@@ -26,4 +26,28 @@ def solution(users, emoticons):
         for d in range(min_discount, max_discount+10,10) :
             stack.append(simul + [d])
     #print(max_discount, min_discount)
+    
+    for c in range(len(com)) :
+        answer2 = [0, 0]
+        for u in users :
+            bill = 0
+            for i in range(len(emoticons)) :
+                #할인율이 사용자가 원하는 할인율보다 크면 결제금액에 추가
+                if u[0] <= com[c][i] :
+                    bill += emoticons[i] * (100 - com[c][i]) / 100
+            #할인하는 이모티콘 모두 구매하기 위한 결제금액이 자신의 기준가를 넘어가면
+            #이모티콘 플러스 서비스를 가입함
+            if bill >= u[1] :
+                answer2[0] += 1
+            #기준가를 안넘어가면 그냥 이모티콘을 별도 구매하는 것이기 때문에
+            #총 이모티콘 판매액이 그만큼 증가함
+            else :
+                answer2[1] += int(bill)
+            #print(bill)
+            #각 조합마다 가입자의 수와 이모티콘 판매액을 모두 비교해서 목표 1, 2번에 부합하는 답을 추출함
+            if answer2[0] > answer[0] or (answer2[0] == answer[0] and answer2[1] > answer[1]) :
+                answer = answer2
+            
+                
+                
     return answer
